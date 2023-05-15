3b5d92336879b3516b55852f2fcf9e076c42e38e
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-05-16 00:18:00 +0900

    q2

--- week08/hookstest/Hooks/src/hooks/1.basic/q2.js
+++ week08/hookstest/Hooks/src/hooks/1.basic/q2.js
@@ -1,13 +1,22 @@
 import { useRef, useState } from 'react';
 
 function Q2() {
-  const arr = [];
-  const CopyArr = useRef();
   const [forceRender, setForceRender] = useState(false);
-  const [hide, setHide] = useState(true);
-  const List = useRef(arr);
   const [input, setInput] = useState('');
+  const [hide, setHide] = useState(true);
   const isSubmit = useRef(false);
+  const changRef = useRef();
+  // 추가가 됐는지 확인해주는 용도
+  const SubmitList = useRef();
+  // 제출된 리스트
+  const List = useRef(arr);
+  // 제출이 안된 리스트
+  const arr = [];
+  const randomColor = () => {
+    return Math.floor(Math.random() * 255);
+  };
+  // const [hide,setHide] = useState(true) 제출버튼 눌렀을 때, 안눌렀을 때 useRef에서 제출된 리스트를 보여줄지 제출이 안된 리스트를 보여줄지
+
   const onAddList = () => {
     if (input === '') return;
     setForceRender((prev) => !prev);
@@ -23,7 +32,7 @@ function Q2() {
   };
 
   const submitBtn = () => {
-    CopyArr.current = [...List.current];
+    SubmitList.current = [...List.current];
     setHide(false);
     isSubmit.current = false;
   };
@@ -52,11 +61,17 @@ function Q2() {
         </p>
         <p>
           <button onClick={submitBtn}>제출</button>
+          {/*
+            {hide ? (CopyArr.current===undefined ? null:CopyArr.current.map((v)=> {return <div>{v}</div>})):
+            List.current.length ===0 ? <div>제출할 목록이 없습니다</div>:
+            List.current.map((v)=>{return <div>{v}</div>})}
+          */}
+          {/* 제출 버튼을 처음 눌렀을 때 추가된 리스트가 있음에도 추가되지 않음을 해결하였습니다. */}
         </p>
-        {CopyArr.current === undefined || CopyArr.current.length === 0 ? (
+        {SubmitList.current === undefined || SubmitList.current.length === 0 ? (
           <div>제출할 목록이 없습니다</div>
         ) : (
-          CopyArr.current.map((v) => {
+          SubmitList.current.map((v) => {
             return <div>{v}</div>;
           })
         )}
@@ -64,10 +79,10 @@ function Q2() {
       </div>
       <div>
         <h2>문제 2-2</h2>
-        <p ref={chgColor}> 이 문구는 아래 버튼을 누르면 색상이 바뀝니다</p>
+        <p ref={changRef}> 이 문구는 아래 버튼을 누르면 색상이 바뀝니다</p>
         <button
           onClick={() => {
-            textColor();
+            changRef.current.style.color = `rgb(${randomColor()},${randomColor()},${randomColor()})`;
           }}
         >
           변경
